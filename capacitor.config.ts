063e33849fc7696e5e2707070c95e4b7ece09018
Heaven Travis <appmanagement.ht@gmail.com>
2025-12-26 15:39:18 -0500

    Fix Radix UI dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.skillcraftnextgen.app',
+  appName: 'SkillCraft NextGen',
+  webDir: 'dist'
+};
+
+export default config;
